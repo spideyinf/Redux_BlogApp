7c3378b6ac1cb668d955e9236da868a1b13c1954
Hoang Phuc <phuchoang.inf@gmail.com>
2018-05-29 15:08:31 +0700

    Add PostsShow function

--- src/reducers/PostsReducer.js
+++ src/reducers/PostsReducer.js
@@ -1,11 +1,16 @@
-import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import _ from 'lodash'
+import { FETCH_POSTS, FETCH_POST } from '../actions'
 
-export default function (state = {}, action) {
+export default function(state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:
+      return {
+        ...state,
+        [action.payload.data.id]: action.payload.data,
+      }
     case FETCH_POSTS:
-      return _.mapKeys(action.payload.data, 'id');
+      return _.mapKeys(action.payload.data, 'id')
     default:
-      return state;
+      return state
   }
 }
